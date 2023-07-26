bb1f0bbecdf3464f78fb4a0048a787962bdc3eab
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2023-07-26 11:20:26 +0200

    chore(deps): Updated   (#318)
    Triggered by https://github.com/cdktf/cdktf-repository-manager/actions/runs/5666717022

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -14,7 +14,7 @@ import * as cdktf from 'cdktf';
 export interface HttpProviderConfig {
   /**
   * Alias name
-  * 
+  *
   * Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.4.0/docs#alias HttpProvider#alias}
   */
   readonly alias?: string;
